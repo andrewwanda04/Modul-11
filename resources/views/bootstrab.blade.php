d6c8f98339a9d56ec1a1fe80d6af0d512f6fc994
andrewwanda04 <ayakashibana@gmail.com>
2024-11-28 23:36:13 +0700

    Add files via upload

---
+++ resources/views/bootstrab.blade.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@andrewwandaa • Instagram photos and videos </title>
+        <link rel="stylesheet" href="style.css" type="text/css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+
+    </head>
+    <body>
+        <nav class="navbar navbar-light sticky-top bg-light">
+        <a class="navbar-brand" href="#"><i class="fab fa-instagram"></i> Instagram</a>
+        <ul class="nav">
+            <li class="nav-item">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjSKoyOjhKTNOkbuXv8zhtxMwtpt39UaMmLA&s" alt="pfp" width="10" class="profile-img">
+            </li>
+            <ul class="nav">
+                <li class="nav-item">
+                    <h4>andrewwandaa</h4>
+                </li>
+                <li class="nav-item">
+                    <button type="button" class="btn btn-primary btn-md btn-success"data-toggle="modal" data-target="#exampleModal">Create a new post</button>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-cogs"></i></a>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Log Out</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- Modal Begin -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+              <div class="modal-body ">
+                  <textarea class="form-control" aria-label="With textarea"></textarea>
+                  <!--<input class="form-control mr-sm-2 custom-search-modal" type="search" placeholder="" aria-label="Search">-->
+                  <i class="fas fa-camera-retro p-1 border rounded mt-1" ></i>
+                  <i class="fas fa-map-marker-alt p-1 border rounded mt-1" ></i>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-success btn-sm">Publish</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- modal end -->
+
+        <!-- Tab panes -->
+              <div class="tab-content">
+                <div id="home" class="container-fluid tab-pane active "><br>
+                    <div class="row justify-content-center">
+                        <div class="">
+                            <div class="card mx-auto custom-card" id="prova">
+                                <div class="row post-header col-12 py-2 px-3">
+                                    <div class="col-6 float-left "><h4>andrewwandaa • 7d</h4></div>
+                                </div>
+                                <img class="card-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUTnI5FV3ZKK1y22B6HjL9taykG6UUnN3mEA&s" alt="car">
+                                <div class="card-body px-3">
+                                    <h5 class="card-title">10 likes</h5>
+                                    <p class="card-text">me irl</p>
+                                </div>
+                                 <div class="row post-header px-3 pb-3">
+                                    <div class="col-1 float-left text-left"><i class="far fa-heart"></i></i></div>
+                                    <div class="col-10 float-left text-left">Comment...</div>
+                                    <div class="col-1 float-right text-right"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></div>
+                                </div>
+                            </div>
+
+                            <div class="card mx-auto custom-card mt-3" id="prova">
+                                <div class="row post-header col-12 py-2 px-3">
+                                    <div class="col-6 float-left "><h4>andrewwandaa • 9d</h4></div>
+                                </div>
+                                <img class="card-img" src="https://www.masakapahariini.com/wp-content/uploads/2023/03/shutterstock_1949306203.jpg" alt="geprek">
+                                <div class="card-body px-3">
+                                    <h5 class="card-title">1.122.334 likes</h5>
+                                    <p class="card-text">ayam geprek</p>
+                                </div>
+                                 <div class="row post-header px-3 pb-3">
+                                    <div class="col-1 float-left text-left"><i class="far fa-heart"></i></i></div>
+                                    <div class="col-10 float-left text-left">Comment...</div>
+                                    <div class="col-1 float-right text-right"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></div>
+                                </div>
+                            </div>
+
+                            <div class="card mx-auto custom-card mt-3" id="prova">
+                                <div class="row post-header col-12 py-2 px-3">
+                                    <div class="col-6 float-left "><h4>andrewwandaa • 10d</h4></div>
+                                </div>
+                                <img class="card-img" src="https://nagantour.com/wp-content/uploads/2024/02/Gunung-Bromo.webp" alt="bromo">
+                                <div class="card-body px-3">
+                                    <h5 class="card-title">41 likes</h5>
+                                    <p class="card-text">dingin banget jir</p>
+                                </div>
+                                 <div class="row post-header px-3 pb-3">
+                                    <div class="col-1 float-left text-left"><i class="far fa-heart"></i></i></div>
+                                    <div class="col-10 float-left text-left">Comment...</div>
+                                    <div class="col-1 float-right text-right"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></div>
+                                </div>
+                            </div>
+
+                            <div class="card mx-auto custom-card mt-3" id="prova">
+                                <div class="row post-header col-12 py-2 px-3">
+                                    <div class="col-6 float-left "><h4>andrewwandaa • 21d</h4></div>
+                                </div>
+                                <img class="card-img" src="https://i.ytimg.com/vi/t6qvNJgJSaM/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAtgclJFfxOnSAD3bL3HadvIzoXzA" alt="gold">
+                                <div class="card-body px-3">
+                                    <h5 class="card-title">727 likes</h5>
+                                    <p class="card-text">GOLD GOLD GOLD</p>
+                                </div>
+                                 <div class="row post-header px-3 pb-3">
+                                    <div class="col-1 float-left text-left"><i class="far fa-heart"></i></i></div>
+                                    <div class="col-10 float-left text-left">Comment...</div>
+                                    <div class="col-1 float-right text-right"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+</html>
